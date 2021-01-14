ce91b901a1dbc1728869d18e624c871ef3e44f6b
eoren <eoren@ebay.com>
2021-01-14 18:59:09 +0200

    first

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -8,3 +9,10 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+*/
+
+
+const { Telegraf } = require('telegraf')
+const bot = new Telegraf("1566683413:AAFjg2seF025MmNyic64TLd_NBMTSytOdaM")
+bot.hears('hi', (ctx) => ctx.reply('Hey there fellow UMS!!!'))
+bot.launch()
