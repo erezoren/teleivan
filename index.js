6bf2676182b4048634f5cfc708623bc5062434f3
eoren <eoren@ebay.com>
2021-01-14 20:39:05 +0200

    deletions

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+require('dotenv').config();
 const { Telegraf } = require('telegraf')
-const bot = new Telegraf("1566683413:AAFjg2seF025MmNyic64TLd_NBMTSytOdaM")
-bot.hears('hi', (ctx) => ctx.reply('Hey there fellow UMS!!!'))
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('thanks', (ctx) => ctx.reply('Your\'e more then welcome!'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there UMS Fellow!!!!'))
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('hipster', Telegraf.reply('λ'))
 bot.launch()
+
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
