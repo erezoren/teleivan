c287ea29fd52061be2dbf444cc58f656db876514
eoren <eoren@ebay.com>
2021-01-15 17:44:11 +0200

    relaunching bot on failure

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const { Telegraf } = require('telegraf')
+const {Telegraf} = require('telegraf');
+const axios = require('axios');
 let bot
 if (process.env.NODE_ENV === 'production') {
   bot = new Telegraf(process.env.BOT_TOKEN);
@@ -8,27 +9,66 @@ if (process.env.NODE_ENV === 'production') {
   bot = new Telegraf(process.env.BOT_TOKEN);
 }
 
+bot.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log('Response time: %sms', ms)
+})
 
-bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.catch((err, ctx) => {
+  console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
+})
+bot.start((ctx) => {
+  throw new Error('Example error')
+})
+
+bot.on('sticker', (ctx) => {
+  ctx.reply('👍')
+})
 bot.hears('thanks', (ctx) => ctx.reply('Your\'e more then welcome!'))
 bot.hears('תודה', (ctx) => ctx.reply('אין על מה!!!'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there UMS Fellow!!!!'))
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('hipster', Telegraf.reply('λ'))
+bot.command('stock', (ctx) => {
+  axios
+  .get(
+      `https://mboum.com/api/v1/qu/quote/?symbol=EBAY&${process.env.STOCKS_API_KEY}`)
+  .then(response => {
+    const stockData = response.data[0];
+    let trend;
+    if (stockData.regularMarketChange = 0) {
+      trend = '😜'
+    }
+    else {
+      trend = '😭'
+    }
+    ctx.telegram.sendMessage(ctx.message.chat.id, `${trend} ${stockData.ask}`)
+  })
+  .catch(error => {
+    ctx.telegram.sendMessage(ctx.message.chat.id, 'error ' + error)
+  });
+})
+
+
+const regex = new RegExp(/נעם*/)
+bot.hears(regex, (ctx) => {
+  ctx.reply('בדיוק גם אני חשבתי על נעם....איזה קטע! 😻😻😻')
+});
+
+bot.on('ssss', (ctx) => {
+  ctx.telegram.sendMessage(ctx.message.chat.id,
+      `Hello ${ctx.update.message.from.first_name}`)
+})
 bot.launch()
 
-
-
 // Enable graceful stop
-process.once('SIGINT', () =>{
+process.once('SIGINT', () => {
   bot.stop('SIGINT')
-} )
+})
 process.once('SIGTERM', () => {
   bot.stop('SIGTERM')
   bot.launch()
 })
-
-
 
 const express = require('express')
 const bodyParser = require('body-parser');
