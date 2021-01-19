67e3d1c652ccd15fd5f02ecd71bdaaa1568c9647
eoren <eoren@ebay.com>
2021-01-19 14:34:38 +0200

    gif added

--- commands/externalApiCommands.js
+++ commands/externalApiCommands.js
@@ -1,10 +1,11 @@
 const axios = require('axios');
+require('dotenv').config();
 
 function bindCommands(bot) {
   bot.command('stock', (ctx) => {
     axios
     .get(
-        `https://mboum.com/api/v1/qu/quote/?symbol=EBAY&${process.env.STOCKS_API_KEY}`)
+        `${process.env.STOCK_BASE_URL}${process.env.STOCKS_API_KEY}`)
     .then(response => {
       const stockData = response.data[0];
       let trend;
@@ -23,11 +24,25 @@ function bindCommands(bot) {
 
   bot.command('chuck', (ctx) => {
     axios
-    .get(
-        `https://api.chucknorris.io/jokes/random`)
+    .get(process.env.CHUCK_BASE_URL)
     .then(response => {
       const data = response.data;
       ctx.telegram.sendMessage(ctx.message.chat.id, data.value)
+    })
+    .catch(error => {
+      ctx.telegram.sendMessage(ctx.message.chat.id, 'error ' + error)
+    });
+  })
+
+  bot.command('gif', (ctx) => {
+    const query = encodeURI(ctx.message.text.replace('/gif ', ''))
+    axios
+    .get(`${process.env.GIPHY_BASE_URL}${query}`)
+    .then(response => {
+      const data = response.data.data;
+      const image = data[0].images.original.mp4;
+      //ctx.replyWithVideo({source: image})
+      ctx.telegram.sendMessage(ctx.message.chat.id, image)
     })
     .catch(error => {
       ctx.telegram.sendMessage(ctx.message.chat.id, 'error ' + error)
