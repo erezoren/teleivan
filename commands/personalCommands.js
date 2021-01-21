c184755a9d2481597e62a69008ae6d783462f60a
eoren <eoren@ebay.com>
2021-01-21 07:25:12 +0200

    added meital

--- commands/personalCommands.js
+++ commands/personalCommands.js
@@ -1,8 +1,13 @@
 
 function bindCommands(bot) {
-  const regex = new RegExp(/נועם*|נעם*/)
-  bot.hears(regex, (ctx) => {
+  const noamRegex = new RegExp(/נועם*|נעם*/)
+  bot.hears(noamRegex, (ctx) => {
     ctx.reply('בדיוק גם אני חשבתי על נעם....איזה קטע! 😻😻😻')
+  });
+
+  const meitalRegex = new RegExp(/מיטל*|מטל*/)
+  bot.hears(meitalRegex, (ctx) => {
+    ctx.reply('גם כן מיטל הזאת עפה על עצמה. שונאת בוטים מוצהרת! 😡')
   });
 }
 
